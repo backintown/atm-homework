13d7b02a58b62c90342851b8c8d05fa2347ce1e6
Alex Cheng <acheng@datto.com>
2019-05-06 18:49:33 -0400

    add test

--- tests/Account.test.js
+++ tests/Account.test.js
@@ -15,3 +15,11 @@ test('withdraw amount from account', () => {
     testAccount.appendTransaction({ date: '05/08/2019', amount: -100 });
     expect(testAccount.balance).toBe(400);
 });
+
+test('get daily withdrawal amount', () => {
+    let testAccount = new Account(uuid(), 0);
+    testAccount.appendTransaction({ date: '05/05/2019', amount: 5000 });
+    testAccount.appendTransaction({ date: '05/05/2019', amount: -100 });
+    testAccount.appendTransaction({ date: '05/06/2019', amount: -1900 });
+    expect(testAccount.getDailyTotalWithdrawal(new Date(2019,4,6))).toBe(1900);
+})
